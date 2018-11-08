817d8ef96b578bde552ccfdcfebe5bb3506a059b
Amanda Santoro <mandy.santoro@gmail.com>
2018-11-08 14:24:45 -0800

    altered names and some datatypes for consistency

--- hotelSkiResort.sql
+++ hotelSkiResort.sql
@@ -1,5 +1,3 @@
--- updated the size of notes in titles relation// Hazra
-
 drop table bookedlessons;
 drop table lessontime;
 drop table lesson;
@@ -18,7 +16,6 @@ drop table room;
 drop table member;
 drop table customer;
 
-
 commit;
 
 
@@ -28,19 +25,19 @@ create table customer
 	e_mail varchar(40) not null,
 	creditcard_num char(16) null,
 	primary key (c_id));
- 
+
 grant select on customer to public;
 
 create table member
     (c_id int not null,
 	fee float null,
 	points int null,
-	join_date varchar(20) null,
+	join_date char(8) null,
 	primary key (c_id),
 	foreign key (c_id) references customer ON DELETE CASCADE);
- 
+
 grant select on member to public;
- 
+
 
 create table room
     (room_num int not null,
@@ -49,125 +46,125 @@ create table room
 	primary key (room_num));
 
  grant select on room to public;
-	
 
- create table roomrate
+
+ create table roomRate
 	(room_type char(10) not null,
 	room_rate float null,
 	primary key (room_type),
-	foreign key (room_type) references room ON DELETE CASCADE); 
+	foreign key (room_type) references room ON DELETE CASCADE);
 
-grant select on roomrate to public;
- 
+grant select on roomRate to public;
 
-create table roomreservation
+
+create table roomReservation
     (confirm_num int not null,
 	room_num int not null,
 	c_id int not null,
 	primary key (confirm_num),
 	foreign key (room_num) references room ON DELETE CASCADE,
 	foreign key (c_id) references customer ON DELETE CASCADE);
- 
-grant select on roomreservation to public;
- 
-create table hotelstaff
+
+grant select on roomReservation to public;
+
+create table hotelStaff
     (staff_id int not null,
 	s_name varchar(20) not null,
-	phone char(10) null,	
+	phone char(10) null,
 	primary key (staff_id));
- 
-grant select on hotelstaff to public;
 
-create table skistaff
+grant select on hotelStaff to public;
+
+create table skiStaff
     (staff_id int not null,
 	s_name varchar(20) null,
-	phone char(10) null,	
+	phone char(10) null,
 	primary key (staff_id));
- 
-grant select on skistaff to public;
 
-create table rentalequip
+grant select on skiStaff to public;
+
+create table rentalEquip
     (equip_id int not null,
 	 equip_type varchar(20) not null,
-	rental_rate float not null,	
+	rental_rate float not null,
 	primary key (equip_id));
- 
-grant select on rentalequip to public;
+
+grant select on rentalEquip to public;
 
 create table rentalEquipRate
     (equip_type varchar(20) not null,
 	rental_rate	float not null,
 	primary key (equip_type),
-	foreign key (equip_type) references rentalequip  ON DELETE CASCADE);
- 
+	foreign key (equip_type) references rentalEquip  ON DELETE CASCADE);
+
 grant select on rentalEquipRate to public;
 
-create table equipreservation
+create table equipReservation
     (confirm_num int not null,
 	equip_id int not null,
 	c_id int not null,
 	primary key (confirm_num),
-	foreign key (equip_id) references rentalequip ON DELETE CASCADE,
+	foreign key (equip_id) references rentalEquip ON DELETE CASCADE,
 	foreign key (c_id) references customer ON DELETE CASCADE);
- 
-grant select on equipreservation to public;
 
-create table roomManagement  
+grant select on equipReservation to public;
+
+create table roomManagement
     (room_num int not null,
 	 staff_id int not null,
 	primary key (room_num, staff_id),
 	foreign key (room_num) references room ON DELETE CASCADE,
-	foreign key (staff_id) references hotelstaff ON DELETE CASCADE);
- 
+	foreign key (staff_id) references hotelStaff ON DELETE CASCADE);
+
 grant select on roomManagement to public;
 
-create table equipManagement 
+create table equipManagement
     (equip_id int not null,
 	 staff_id int not null,
 	primary key (equip_id, staff_id),
-	foreign key (equip_id) references rentalequip ON DELETE CASCADE,
-	foreign key (staff_id) references skistaff ON DELETE CASCADE);
- 
+	foreign key (equip_id) references rentalEquip ON DELETE CASCADE,
+	foreign key (staff_id) references skiStaff ON DELETE CASCADE);
+
 grant select on equipManagement to public;
 
 create table purchasedLiftPass
     (c_id int not null,
 	 pass_id int not null,
-	purchase_date datetime not null,
-	pass_price float not null,	
+	purchase_date char(8) not null,
+	pass_price float not null,
 	primary key (c_id, pass_id),
 	foreign key (c_id) references customer ON DELETE CASCADE);
- 
+
 grant select on purchasedLiftPass to public;
 
 create table lesson
     (staff_id int not null,
-	lesson_datetime datetime not null,
-	lesson_type varchar(20) not null,	
+	lesson_datetime char(12) not null,
+	lesson_type varchar(20) not null,
 	primary key (lesson_type, staff_id),
-	foreign key (staff_id) references skistaff ON DELETE CASCADE);
- 
+	foreign key (staff_id) references skiStaff ON DELETE CASCADE);
+
 grant select on lesson to public;
 
-create table lessontime
+create table lessonTime
     (lesson_type varchar(20) not null,
-	lesson_datetime datetime not nul,
+	lesson_datetime char(12) not nul,
 	primary key (lesson_type),
 	foreign key (lesson_type) references lesson ON DELETE CASCADE,
 	foreign key (lesson_datetime) references lesson ON DELETE CASCADE);
- 
-grant select on lessontime to public;
 
-create table bookedlessons 
+grant select on lessonTime to public;
+
+create table bookedLessons
     (c_id int not null,
 	lesson_type varchar(20) not null,
 	staff_id int not null,
 	primary key (c_id, lesson_type, staff_id),
 	foreign key (c_id) references customer ON DELETE CASCADE,
-	foreign key (staff_id) references skistaff ON DELETE CASCADE,
+	foreign key (staff_id) references skiStaff ON DELETE CASCADE,
 	foreign key (lesson_type) references lesson ON DELETE CASCADE);
- 
-grant select on bookedlessons to public;
+
+grant select on bookedLessons to public;
 
 
 
