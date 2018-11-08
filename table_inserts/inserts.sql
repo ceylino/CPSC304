af7a5bf349b2f29c9a74db989f0280fe483e0023
ceylino <ceylin_ozdemir@hotmail.com>
2018-11-08 13:37:51 -0800

    Update inserts.sql

--- table_inserts/inserts.sql
+++ table_inserts/inserts.sql
@@ -231,3 +231,193 @@ VALUES ("helmet", 5.00);
 commit;
 
 /*---------------------------------------------------------------------------------*/
+
+/*EQUIPRESERVATION TABLE - 7 entries*/
+INSERT INTO EquipReservation (confirmation_num, equip_id, customer_id)
+VALUES (1, 1, 1);
+
+INSERT INTO EquipReservation (confirmation_num, equip_id, customer_id)
+VALUES (2, 9, 1);
+
+INSERT INTO EquipReservation (confirmation_num, equip_id, customer_id)
+VALUES (3, 10, 1);
+
+INSERT INTO EquipReservation (confirmation_num, equip_id, customer_id)
+VALUES (4, 14, 1);
+
+INSERT INTO EquipReservation (confirmation_num, equip_id, customer_id)
+VALUES (5, 3, 10)
+
+INSERT INTO EquipReservation (confirmation_num, equip_id, customer_id)
+VALUES (6, 5, 7)
+
+INSERT INTO EquipReservation (confirmation_num, equip_id, customer_id)
+VALUES (7, 12, 4)
+
+/*---------------------------------------------------------------------------------*/
+/*ROOMRESERVATION TABLE - 7 entries*/
+INSERT INTO RoomReservation (confirmation_num, room_num, customer_id)
+VALUES (1, 1, 4);
+
+INSERT INTO RoomReservation (confirmation_num, room_num, customer_id)
+VALUES (2, 8, 4);
+
+INSERT INTO RoomReservation (confirmation_num, room_num, customer_id)
+VALUES (3, 10, 2);
+
+INSERT INTO RoomReservation (confirmation_num, room_num, customer_id)
+VALUES (4, 11, 10);
+
+INSERT INTO RoomReservation (confirmation_num, room_num, customer_id)
+VALUES (5, 13, 8);
+
+INSERT INTO RoomReservation (confirmation_num, room_num, customer_id)
+VALUES (6, 14, 6);
+
+INSERT INTO RoomReservation (confirmation_num, room_num, customer_id)
+VALUES (7, 15, 5);
+
+/*---------------------------------------------------------------------------------*/
+/*ROOMMANAGEMENT TABLE - 7 entries (1 for each reserved room)*/
+INSERT INTO RoomManagement (room_num, staff_id)
+VALUES (1, 4);
+
+INSERT INTO RoomManagement (room_num, staff_id)
+VALUES (2, 3);
+
+INSERT INTO RoomManagement (room_num, staff_id)
+VALUES (3, 5);
+
+INSERT INTO RoomManagement (room_num, staff_id)
+VALUES (4, 2);
+
+INSERT INTO RoomManagement (room_num, staff_id)
+VALUES (5, 1);
+
+INSERT INTO RoomManagement (room_num, staff_id)
+VALUES (6, 1);
+
+INSERT INTO RoomManagement (room_num, staff_id)
+VALUES (7, 3);
+
+
+/*---------------------------------------------------------------------------------*/
+/*EQUIPMANAGEMENT TABLE - 5 entries*/
+INSERT INTO EquipManagement (equip_id, staff_id)
+VALUES (1, 6);
+
+INSERT INTO EquipManagement (equip_id, staff_id)
+VALUES (2, 6);
+
+INSERT INTO EquipManagement (equip_id, staff_id)
+VALUES (3, 6);
+
+INSERT INTO EquipManagement (equip_id, staff_id)
+VALUES (4, 6);
+
+INSERT INTO EquipManagement (equip_id, staff_id)
+VALUES (5, 8);
+
+INSERT INTO EquipManagement (equip_id, staff_id)
+VALUES (6, 7);
+
+INSERT INTO EquipManagement (equip_id, staff_id)
+VALUES (7, 0);
+
+/*---------------------------------------------------------------------------------*/
+/*PURCHASEDLIFTPASS TABLE - 6 entries*/
+INSERT INTO PurchasedLiftpass (customer_id, pass_id, purchase_date, pass_price)
+VALUES (1, 1, 20180809, 50.00);
+
+INSERT INTO PurchasedLiftpass (customer_id, pass_id, purchase_date, pass_price)
+VALUES (4, 2, 201801205, 50.00);
+
+INSERT INTO PurchasedLiftpass (customer_id, pass_id, purchase_date, pass_price)
+VALUES (9, 3, 20181012, 50.00);
+
+INSERT INTO PurchasedLiftpass (customer_id, pass_id, purchase_date, pass_price)
+VALUES (7, 4, 20180901, 50.00);
+
+INSERT INTO PurchasedLiftpass (customer_id, pass_id, purchase_date, pass_price)
+VALUES (10, 5, 20171019, 50.00);
+
+INSERT INTO PurchasedLiftpass (customer_id, pass_id, purchase_date, pass_price)
+VALUES (12, 6, 20050105, 50.00);
+
+/*---------------------------------------------------------------------------------*/
+/*LESSON TABLE - 9 entries 
+Not quite sure how we should represent date and time together*/
+INSERT INTO Lesson (type, staff_id, lesson_datetime)
+VALUES ("ski", 6, 20180809 11:30:00);
+
+INSERT INTO Lesson (type, staff_id, lesson_datetime)
+VALUES ("ski", 6, 20180809 14:30:00);
+
+INSERT INTO Lesson (type, staff_id, lesson_datetime)
+VALUES ("ski", 8, 201801205 13:00:00);
+
+INSERT INTO Lesson (type, staff_id, lesson_datetime)
+VALUES ("ski", 8, 201801206 13:00:00);
+
+INSERT INTO Lesson (type, staff_id, lesson_datetime)
+VALUES ("snowboard", 0, 20180903 15:15:00);
+
+INSERT INTO Lesson (type, staff_id, lesson_datetime)
+VALUES ("snowboard", 0, 20180904 15:15:00);
+
+INSERT INTO Lesson (type, staff_id, lesson_datetime)
+VALUES ("snowboard", 7, 20171019 12:45:00);
+
+INSERT INTO Lesson (type, staff_id, lesson_datetime)
+VALUES ("snowboard", 7, 20171019 15:45:00);
+
+INSERT INTO Lesson (type, staff_id, lesson_datetime)
+VALUES ("crosscountry", 9, 20151030 9:00:00);
+
+/*---------------------------------------------------------------------------------*/
+/*LESSONTIME TABLE - 9 entries*/
+INSERT INTO LessonTime (type, lesson_datetime)
+VALUES ("ski", 20180809 11:30:00);
+
+INSERT INTO LessonTime (type, lesson_datetime)
+VALUES ("ski", 20180809 14:30:00);
+
+INSERT INTO LessonTime (type, lesson_datetime)
+VALUES ("ski", 201801205 13:00:00);
+
+INSERT INTO LessonTime (type, lesson_datetime)
+VALUES ("ski", 201801206 13:00:00);
+
+INSERT INTO LessonTime (type, lesson_datetime)
+VALUES ("snowboard", 20180903 15:15:00);
+
+INSERT INTO LessonTime (type, lesson_datetime)
+VALUES ("snowboard", 20180904 15:15:00);
+
+INSERT INTO LessonTime (type, lesson_datetime)
+VALUES ("snowboard", 20171019 12:45:00);
+
+INSERT INTO LessonTime (type, lesson_datetime)
+VALUES ("snowboard", 20171019 15:45:00);
+
+INSERT INTO LessonTime (type, lesson_datetime)
+VALUES ("crosscountry", 20151030 9:00:00);
+
+/*---------------------------------------------------------------------------------*/
+/*BOOKEDLESSONS TABLE - 5 entries*/
+INSERT INTO BookedLessons (customer_id, type, staff_id)
+VALUES (1, "ski", 6);
+
+INSERT INTO BookedLessons (customer_id, type, staff_id)
+VALUES (6, "snowboard", 0);
+
+INSERT INTO BookedLessons (customer_id, type, staff_id)
+VALUES (4, "snowboard", 0);
+
+INSERT INTO BookedLessons (customer_id, type, staff_id)
+VALUES (10, "ski", 8);
+
+INSERT INTO BookedLessons (customer_id, type, staff_id)
+VALUES (14, "crosscountry", 9);
+
+/*---------------------------------------------------------------------------------*/
