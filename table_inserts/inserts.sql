ff01c40513510a252cacc24f944fecd89cd9c865
Amanda Santoro <mandy.santoro@gmail.com>
2018-11-09 09:26:32 -0800

    small changes to add naming consistency

--- table_inserts/inserts.sql
+++ table_inserts/inserts.sql
@@ -104,109 +104,109 @@ commit;
 
 /*---------------------------------------------------------------------------------*/
 /*ROOM RATE TABLE - 6 entries (1 for each of the 6 room types)*/
-INSERT INTO roomrate (room_type, room_rate)
+INSERT INTO roomRate (room_type, room_rate)
 VALUES ("double", 150.00);
 
-INSERT INTO RoomRate (room_type, room_rate)
+INSERT INTO roomRate (room_type, room_rate)
 VALUES ("triple", 150.00);
 
-INSERT INTO RoomRate (room_type, room_rate)
+INSERT INTO roomRate (room_type, room_rate)
 VALUES ("quad", 150.00);
 
-INSERT INTO RoomRate (room_type, room_rate)
+INSERT INTO roomRate (room_type, room_rate)
 VALUES ("king", 150.00);
 
-INSERT INTO RoomRate (room_type, room_rate)
+INSERT INTO roomRate (room_type, room_rate)
 VALUES ("honeymoon", 450.00);
 
-INSERT INTO RoomRate (room_type, room_rate)
+INSERT INTO roomRate (room_type, room_rate)
 VALUES ("president", 500.00);
 
 commit;
 
 /*---------------------------------------------------------------------------------*/
 /*HOTEL STAFF TABLE - 5 entries*/
-INSERT INTO hotelstaff (staff_id, s_name, phone)
+INSERT INTO hotelStaff (staff_id, s_name, phone)
 VALUES (1, "Zayn", "6041234567");
 
-INSERT INTO hotelstaff (staff_id, s_name, phone)
+INSERT INTO hotelStaff (staff_id, s_name, phone)
 VALUES (2, "Yasmin", "6041250080");
 
-INSERT INTO hotelstaff (staff_id, s_name, phone)
+INSERT INTO hotelStaff (staff_id, s_name, phone)
 VALUES (3, "Xander", "6041230000");
 
-INSERT INTO hotelstaff (staff_id, s_name, phone)
+INSERT INTO hotelStaff (staff_id, s_name, phone)
 VALUES (4, "Walter", "6041298800");
 
-INSERT INTO hotelstaff (staff_id, s_name, phone)
+INSERT INTO hotelStaff (staff_id, s_name, phone)
 VALUES (5, "Violet", "6049019977");
 
 commit;
 
 /*---------------------------------------------------------------------------------*/
 /*SKI STAFF TABLE - 5 entries*/
-INSERT INTO skistaff (staff_id, s_name, phone)
+INSERT INTO skiStaff (staff_id, s_name, phone)
 VALUES (6, "Ursula", "6042224567");
 
-INSERT INTO skistaff (staff_id, s_name, phone)
+INSERT INTO skiStaff (staff_id, s_name, phone)
 VALUES (7, "Tom", "6043330080");
 
-INSERT INTO skistaff (staff_id, s_name, phone)
+INSERT INTO skiStaff (staff_id, s_name, phone)
 VALUES (8, "Sam", "6044440000");
 
-INSERT INTO skistaff (staff_id, s_name, phone)
+INSERT INTO skiStaff (staff_id, s_name, phone)
 VALUES (9, "Ron", "6045558800");
 
-INSERT INTO skistaff (staff_id, s_name, phone)
+INSERT INTO skiStaff (staff_id, s_name, phone)
 VALUES (0, "Quin", "6049066677");
 
 commit;
 
 /*---------------------------------------------------------------------------------*/
 /*RENTAL EQUIPMENT TABLE - 15 entries*/
-INSERT INTO rentalequip (equip_id, equip_type, rental_rate)
+INSERT INTO rentalEquip (equip_id, equip_type, rental_rate)
 VALUES (1, "skis", 15.00);
 
-INSERT INTO rentalequip (equip_id, equip_type, rental_rate)
+INSERT INTO rentalEquip (equip_id, equip_type, rental_rate)
 VALUES (2, "skis", 15.00);
 
-INSERT INTO rentalequip (equip_id, equip_type, rental_rate)
+INSERT INTO rentalEquip (equip_id, equip_type, rental_rate)
 VALUES (3, "skis", 15.00);
 
-INSERT INTO rentalequip (equip_id, equip_type, rental_rate)
+INSERT INTO rentalEquip (equip_id, equip_type, rental_rate)
 VALUES (4, "snowboard", 10.00);
 
-INSERT INTO rentalequip (equip_id, equip_type, rental_rate)
+INSERT INTO rentalEquip (equip_id, equip_type, rental_rate)
 VALUES (5, "snowboard", 10.00);
 
-INSERT INTO rentalequip (equip_id, equip_type, rental_rate)
+INSERT INTO rentalEquip (equip_id, equip_type, rental_rate)
 VALUES (6, "snowboard", 10.00);
 
-INSERT INTO rentalequip (equip_id, equip_type, rental_rate)
+INSERT INTO rentalEquip (equip_id, equip_type, rental_rate)
 VALUES (7, "boots", 10.00);
 
-INSERT INTO rentalequip (equip_id, equip_type, rental_rate)
+INSERT INTO rentalEquip (equip_id, equip_type, rental_rate)
 VALUES (8, "boots", 10.00);
 
-INSERT INTO rentalequip (equip_id, equip_type, rental_rate)
+INSERT INTO rentalEquip (equip_id, equip_type, rental_rate)
 VALUES (9, "boots", 10.00);
 
-INSERT INTO rentalequip (equip_id, equip_type, rental_rate)
+INSERT INTO rentalEquip (equip_id, equip_type, rental_rate)
 VALUES (10, "poles", 5.00);
 
-INSERT INTO rentalequip (equip_id, equip_type, rental_rate)
+INSERT INTO rentalEquip (equip_id, equip_type, rental_rate)
 VALUES (11, "poles", 5.00);
 
-INSERT INTO rentalequip (equip_id, equip_type, rental_rate)
+INSERT INTO rentalEquip (equip_id, equip_type, rental_rate)
 VALUES (12, "poles", 5.00);
 
-INSERT INTO rentalequip (equip_id, equip_type, rental_rate)
+INSERT INTO rentalEquip (equip_id, equip_type, rental_rate)
 VALUES (13, "helmet", 5.00);
 
-INSERT INTO rentalequip (equip_id, equip_type, rental_rate)
+INSERT INTO rentalEquip (equip_id, equip_type, rental_rate)
 VALUES (14, "helmet", 5.00);
 
-INSERT INTO rentalequip (equip_id, equip_type, rental_rate)
+INSERT INTO rentalEquip (equip_id, equip_type, rental_rate)
 VALUES (15, "helmet", 5.00);
 
 commit;
@@ -233,205 +233,205 @@ commit;
 /*---------------------------------------------------------------------------------*/
 
 /*EQUIPRESERVATION TABLE - 7 entries*/
-INSERT INTO EquipReservation (confirmation_num, equip_id, customer_id)
+INSERT INTO equipReservation (confirm_num, equip_id, c_id)
 VALUES (1, 1, 1);
 
-INSERT INTO EquipReservation (confirmation_num, equip_id, customer_id)
+INSERT INTO equipReservation (confirm_num, equip_id, c_id)
 VALUES (2, 9, 1);
 
-INSERT INTO EquipReservation (confirmation_num, equip_id, customer_id)
-VALUES (3, 10, 1);
+INSERT INTO equipReservation (confirm_num, equip_id, c_id)
+VALUES (3, 10, 3);
 
-INSERT INTO EquipReservation (confirmation_num, equip_id, customer_id)
+INSERT INTO equipReservation (confirm_num, equip_id, c_id)
 VALUES (4, 14, 1);
 
-INSERT INTO EquipReservation (confirmation_num, equip_id, customer_id)
+INSERT INTO equipReservation (confirm_num, equip_id, c_id)
 VALUES (5, 3, 10)
 
-INSERT INTO EquipReservation (confirmation_num, equip_id, customer_id)
+INSERT INTO equipReservation (confirm_num, equip_id, c_id)
 VALUES (6, 5, 7)
 
-INSERT INTO EquipReservation (confirmation_num, equip_id, customer_id)
+INSERT INTO equipReservation (confirm_num, equip_id, c_id)
 VALUES (7, 12, 4)
 
 commit;
 
 /*---------------------------------------------------------------------------------*/
 /*ROOMRESERVATION TABLE - 7 entries*/
-INSERT INTO RoomReservation (confirmation_num, room_num, customer_id)
+INSERT INTO roomReservation (confirm_num, room_num, c_id)
 VALUES (1, 1, 4);
 
-INSERT INTO RoomReservation (confirmation_num, room_num, customer_id)
+INSERT INTO roomReservation (confirm_num, room_num, c_id)
 VALUES (2, 8, 4);
 
-INSERT INTO RoomReservation (confirmation_num, room_num, customer_id)
+INSERT INTO roomReservation (confirm_num, room_num, c_id)
 VALUES (3, 10, 2);
 
-INSERT INTO RoomReservation (confirmation_num, room_num, customer_id)
+INSERT INTO roomReservation (confirm_num, room_num, c_id)
 VALUES (4, 11, 10);
 
-INSERT INTO RoomReservation (confirmation_num, room_num, customer_id)
+INSERT INTO roomReservation (confirm_num, room_num, c_id)
 VALUES (5, 13, 8);
 
-INSERT INTO RoomReservation (confirmation_num, room_num, customer_id)
+INSERT INTO roomReservation (confirm_num, room_num, c_id)
 VALUES (6, 14, 6);
 
-INSERT INTO RoomReservation (confirmation_num, room_num, customer_id)
+INSERT INTO roomReservation (confirm_num, room_num, c_id)
 VALUES (7, 15, 5);
 
 commit;
 
 /*---------------------------------------------------------------------------------*/
-/*ROOMMANAGEMENT TABLE - 7 entries (1 for each reserved room)*/
-INSERT INTO RoomManagement (room_num, staff_id)
+/*ROOMMANAGEMENT TABLE - 7 entries*/
+INSERT INTO roomManagement (room_num, staff_id)
 VALUES (1, 4);
 
-INSERT INTO RoomManagement (room_num, staff_id)
+INSERT INTO roomManagement (room_num, staff_id)
 VALUES (2, 3);
 
-INSERT INTO RoomManagement (room_num, staff_id)
+INSERT INTO roomManagement (room_num, staff_id)
 VALUES (3, 5);
 
-INSERT INTO RoomManagement (room_num, staff_id)
+INSERT INTO roomManagement (room_num, staff_id)
 VALUES (4, 2);
 
-INSERT INTO RoomManagement (room_num, staff_id)
+INSERT INTO roomManagement (room_num, staff_id)
 VALUES (5, 1);
 
-INSERT INTO RoomManagement (room_num, staff_id)
+INSERT INTO roomManagement (room_num, staff_id)
 VALUES (6, 1);
 
-INSERT INTO RoomManagement (room_num, staff_id)
+INSERT INTO roomManagement (room_num, staff_id)
 VALUES (7, 3);
 
 commit;
 
 /*---------------------------------------------------------------------------------*/
 /*EQUIPMANAGEMENT TABLE - 5 entries*/
-INSERT INTO EquipManagement (equip_id, staff_id)
+INSERT INTO equipManagement (equip_id, staff_id)
 VALUES (1, 6);
 
-INSERT INTO EquipManagement (equip_id, staff_id)
+INSERT INTO equipManagement (equip_id, staff_id)
 VALUES (2, 6);
 
-INSERT INTO EquipManagement (equip_id, staff_id)
+INSERT INTO equipManagement (equip_id, staff_id)
 VALUES (3, 6);
 
-INSERT INTO EquipManagement (equip_id, staff_id)
+INSERT INTO equipManagement (equip_id, staff_id)
 VALUES (4, 6);
 
-INSERT INTO EquipManagement (equip_id, staff_id)
+INSERT INTO equipManagement (equip_id, staff_id)
 VALUES (5, 8);
 
-INSERT INTO EquipManagement (equip_id, staff_id)
+INSERT INTO equipManagement (equip_id, staff_id)
 VALUES (6, 7);
 
-INSERT INTO EquipManagement (equip_id, staff_id)
+INSERT INTO equipManagement (equip_id, staff_id)
 VALUES (7, 0);
 
 commit;
 
 /*---------------------------------------------------------------------------------*/
 /*PURCHASEDLIFTPASS TABLE - 6 entries*/
-INSERT INTO PurchasedLiftpass (customer_id, pass_id, purchase_date, pass_price)
-VALUES (1, 1, 20180809, 50.00);
+INSERT INTO purchasedLiftPass (c_id, pass_id, purchase_date, pass_price)
+VALUES (1, 1, "20180809", 50.00);
 
-INSERT INTO PurchasedLiftpass (customer_id, pass_id, purchase_date, pass_price)
-VALUES (4, 2, 201801205, 50.00);
+INSERT INTO purchasedLiftPass (c_id, pass_id, purchase_date, pass_price)
+VALUES (4, 2, 2"01801205", 50.00);
 
-INSERT INTO PurchasedLiftpass (customer_id, pass_id, purchase_date, pass_price)
-VALUES (9, 3, 20181012, 50.00);
+INSERT INTO purchasedLiftPass (c_id, pass_id, purchase_date, pass_price)
+VALUES (9, 3, "20181012", 50.00);
 
-INSERT INTO PurchasedLiftpass (customer_id, pass_id, purchase_date, pass_price)
-VALUES (7, 4, 20180901, 50.00);
+INSERT INTO purchasedLiftPass (c_id, pass_id, purchase_date, pass_price)
+VALUES (7, 4, "20180901", 50.00);
 
-INSERT INTO PurchasedLiftpass (customer_id, pass_id, purchase_date, pass_price)
-VALUES (10, 5, 20171019, 50.00);
+INSERT INTO purchasedLiftPass (c_id, pass_id, purchase_date, pass_price)
+VALUES (10, 5, "20171019", 50.00);
 
-INSERT INTO PurchasedLiftpass (customer_id, pass_id, purchase_date, pass_price)
-VALUES (12, 6, 20050105, 50.00);
+INSERT INTO purchasedLiftPass (c_id, pass_id, purchase_date, pass_price)
+VALUES (10, 6, "20150105", 50.00);
 
 commit;
 
 /*---------------------------------------------------------------------------------*/
-/*LESSON TABLE - 9 entries 
+/*LESSON TABLE - 9 entries
 Not quite sure how we should represent date and time together*/
-INSERT INTO Lesson (type, staff_id, lesson_datetime)
-VALUES ("ski", 6, 20180809 11:30:00);
+INSERT INTO lesson (staff_id, lesson_datetime, lesson_type)
+VALUES (6, "201808091130", "ski");
 
-INSERT INTO Lesson (type, staff_id, lesson_datetime)
-VALUES ("ski", 6, 20180809 14:30:00);
+INSERT INTO lesson (staff_id, lesson_datetime, lesson_type)
+VALUES (6, "201808091430", "ski");
 
-INSERT INTO Lesson (type, staff_id, lesson_datetime)
-VALUES ("ski", 8, 201801205 13:00:00);
+INSERT INTO lesson (staff_id, lesson_datetime, lesson_type)
+VALUES (8, "201812051300", "ski");
 
-INSERT INTO Lesson (type, staff_id, lesson_datetime)
-VALUES ("ski", 8, 201801206 13:00:00);
+INSERT INTO lesson (staff_id, lesson_datetime, lesson_type)
+VALUES (8, 2"018012061300", "ski");
 
-INSERT INTO Lesson (type, staff_id, lesson_datetime)
-VALUES ("snowboard", 0, 20180903 15:15:00);
+INSERT INTO lesson (staff_id, lesson_datetime, lesson_type)
+VALUES (0, "201809031515", "snowboard");
 
-INSERT INTO Lesson (type, staff_id, lesson_datetime)
-VALUES ("snowboard", 0, 20180904 15:15:00);
+INSERT INTO lesson (staff_id, lesson_datetime, lesson_type)
+VALUES (0, "201809041515", "snowboard");
 
-INSERT INTO Lesson (type, staff_id, lesson_datetime)
-VALUES ("snowboard", 7, 20171019 12:45:00);
+INSERT INTO lesson (staff_id, lesson_datetime, lesson_type)
+VALUES (7, "201710191245", "snowboard");
 
-INSERT INTO Lesson (type, staff_id, lesson_datetime)
-VALUES ("snowboard", 7, 20171019 15:45:00);
+INSERT INTO lesson (staff_id, lesson_datetime, lesson_type)
+VALUES (7, "201710191545", "snowboard");
 
-INSERT INTO Lesson (type, staff_id, lesson_datetime)
-VALUES ("crosscountry", 9, 20151030 9:00:00);
+INSERT INTO lesson (staff_id, lesson_datetime, lesson_type)
+VALUES (9, "201510030900", "crosscountry");
 
 commit;
 
 /*---------------------------------------------------------------------------------*/
 /*LESSONTIME TABLE - 9 entries*/
-INSERT INTO LessonTime (type, lesson_datetime)
-VALUES ("ski", 20180809 11:30:00);
+INSERT INTO lessonTime (lesson_type, lesson_datetime)
+VALUES ("ski", "201808091130");
 
-INSERT INTO LessonTime (type, lesson_datetime)
-VALUES ("ski", 20180809 14:30:00);
+INSERT INTO lessonTime (lesson_type, lesson_datetime)
+VALUES ("ski", "201808091430");
 
-INSERT INTO LessonTime (type, lesson_datetime)
-VALUES ("ski", 201801205 13:00:00);
+INSERT INTO lessonTime (lesson_type, lesson_datetime)
+VALUES ("ski", "2018012051300");
 
-INSERT INTO LessonTime (type, lesson_datetime)
-VALUES ("ski", 201801206 13:00:00);
+INSERT INTO lessonTime (lesson_type, lesson_datetime)
+VALUES ("ski", "201812061300");
 
-INSERT INTO LessonTime (type, lesson_datetime)
-VALUES ("snowboard", 20180903 15:15:00);
+INSERT INTO lessonTime (lesson_type, lesson_datetime)
+VALUES ("snowboard", "201809031515");
 
-INSERT INTO LessonTime (type, lesson_datetime)
-VALUES ("snowboard", 20180904 15:15:00);
+INSERT INTO lessonTime (lesson_type, lesson_datetime)
+VALUES ("snowboard", "201809041515");
 
-INSERT INTO LessonTime (type, lesson_datetime)
-VALUES ("snowboard", 20171019 12:45:00);
+INSERT INTO lessonTime (lesson_type, lesson_datetime)
+VALUES ("snowboard", "201710191245");
 
-INSERT INTO LessonTime (type, lesson_datetime)
-VALUES ("snowboard", 20171019 15:45:00);
+INSERT INTO lessonTime (lesson_type, lesson_datetime)
+VALUES ("snowboard", "201710191545");
 
-INSERT INTO LessonTime (type, lesson_datetime)
-VALUES ("crosscountry", 20151030 9:00:00);
+INSERT INTO lessonTime (type, lesson_datetime)
+VALUES ("crosscountry", "201510030900");
 
 commit;
 
 /*---------------------------------------------------------------------------------*/
 /*BOOKEDLESSONS TABLE - 5 entries*/
-INSERT INTO BookedLessons (customer_id, type, staff_id)
+INSERT INTO bookedLessons (c_id, lesson_type, staff_id)
 VALUES (1, "ski", 6);
 
-INSERT INTO BookedLessons (customer_id, type, staff_id)
+INSERT INTO bookedLessons (c_id, lesson_type, staff_id)
 VALUES (6, "snowboard", 0);
 
-INSERT INTO BookedLessons (customer_id, type, staff_id)
+INSERT INTO bookedLessons (c_id, lesson_type, staff_id)
 VALUES (4, "snowboard", 0);
 
-INSERT INTO BookedLessons (customer_id, type, staff_id)
+INSERT INTO bookedLessons (c_id, lesson_type, staff_id)
 VALUES (10, "ski", 8);
 
-INSERT INTO BookedLessons (customer_id, type, staff_id)
-VALUES (14, "crosscountry", 9);
+INSERT INTO bookedLessons (c_id, lesson_type, staff_id)
+VALUES (1, "crosscountry", 9);
 
 commit;
 
